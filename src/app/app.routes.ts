cdb355c744846d27e07c2403caa733dbf7cbde2a
maksof-saim <153712387+maksof-saim@users.noreply.github.com>
2025-11-04 17:47:14 +0500

    add to card work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from '../pages/home/home.component';
 import { AboutComponent } from '../pages/about/about.component';
 import { CategoryComponent } from '../pages/category/category.component';
 import { ContactComponent } from '../pages/contact/contact.component';
+import { CartComponent } from '../components/cart/cart.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, // default route
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'category', component: CategoryComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: 'cart', component: CartComponent },
 ];
